478d0212215907b639d8ce407f1cc36f1f4d561e
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-12-19 22:45:36 +0000

    implementation of texteditview

--- src/lib/AzlTextView.style.ts
+++ src/lib/AzlTextView.style.ts
@@ -1,3 +1,9 @@
 import { css } from 'lit';
 
-export const componentStyle = css``;
+export const componentStyle = css`
+  svg > path {
+    margin: 0 0.5rem;
+    color: var(--text-view-icon-color, #666666);
+    fill: var(--text-view-icon-color, #666666);
+  }
+`;
