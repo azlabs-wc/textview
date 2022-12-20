d2cf6faddee43e560f626064acd26f80b3156305
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-12-20 02:20:26 +0000

    code cleanup

--- src/lib/register.ts
+++ src/lib/register.ts
@@ -3,16 +3,15 @@
  * Safe function to add/define a custom element in the web api
  */
 export function registerElement(name: string, elementClass: any) {
-    if (
-      typeof window !== 'undefined' &&
-      window !== null &&
-      typeof window.customElements !== 'undefined' &&
-      window.customElements !== null
-    ) {
-      const customElement = window.customElements.get(name);
-      if (typeof customElement === 'undefined' || customElement === null) {
-        window.customElements.define(name, elementClass);
-      }
+  if (
+    typeof window !== 'undefined' &&
+    window !== null &&
+    typeof window.customElements !== 'undefined' &&
+    window.customElements !== null
+  ) {
+    const customElement = window.customElements.get(name);
+    if (typeof customElement === 'undefined' || customElement === null) {
+      window.customElements.define(name, elementClass);
     }
   }
-  
+}
